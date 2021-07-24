cd0df5689a6ad36aff3888fc51e46ab1422a8db8
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-07-24 18:37:58 +0530

    simulation start with flippping coin to display heads or tails as winner

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,1 +1,10 @@
-echo"welcome to  flipcoinsimulation"
+#!/bin/bash -x
+echo "Welcome to FlipCoin simulation program"
+coin=$(( RANDOM%2 ))
+echo $coin
+if (( $coin==0 ))
+then
+   echo "Head"
+else
+   echo "Tail"
+fi
